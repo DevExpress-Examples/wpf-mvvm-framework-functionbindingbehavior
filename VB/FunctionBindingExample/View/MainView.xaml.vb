f775bdec56fd6ba15aaa4c336c8e586ba3645886
DevExpressExampleBot <fakeEmail>
2023-11-17 12:03:12 +0400

    Source auto update [skip ci]

--- VB/FunctionBindingExample/View/MainView.xaml.vb
+++ VB/FunctionBindingExample/View/MainView.xaml.vb
@@ -1,17 +1,15 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows.Controls
 
 Namespace FunctionBindingExample.View
+
     ''' <summary>
     ''' Interaction logic for MainView.xaml
     ''' </summary>
-    Partial Public Class MainView
+    Public Partial Class MainView
         Inherits UserControl
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
     End Class
 End Namespace
